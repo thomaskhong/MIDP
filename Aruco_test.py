ca9e8944ba71182b2ec273425ff1f9773d5b48b8
thomaskhong <67075045+thomaskhong@users.noreply.github.com>
2022-02-25 13:11:11 +0000

    Aruco perspective check

--- Aruco_test.py
+++ Aruco_test.py
@@ -3,12 +3,21 @@ import time
 import sys
 import imutils
 from imutils.video import VideoStream
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
 arucoParams = cv2.aruco.DetectorParameters_create()
 
-vs = VideoStream(src=1).start()
+def calc_line_length(point1,point2):
+    line_length = math.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
+    return line_length
+
+vs = VideoStream(src=0).start()
 time.sleep(2.0)
+
+fig = plt.figure()
 
 while True:
     frame = vs.read()
@@ -19,6 +28,12 @@ while True:
     if len(corners) > 0:
         ids = ids.flatten()
 
+        marker_sides = np.zeros((4,1,4))
+        line_deltas = np.zeros((4))
+
+        fig.clear()
+        plt.ylim([0, 30])
+
         for (markerCorner, markerID) in zip(corners, ids):
             corners = markerCorner.reshape((4, 2))
             (topLeft, topRight, bottomRight, bottomLeft) = corners
@@ -28,10 +43,28 @@ while True:
             bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
             topLeft = (int(topLeft[0]), int(topLeft[1]))
 
-            cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
-            cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
-            cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
-            cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+            topLine_length = int(calc_line_length(topLeft,topRight))
+            rightLine_length = int(calc_line_length(topRight,bottomRight))
+            bottomLine_length = int(calc_line_length(bottomLeft,bottomRight))
+            leftLine_length = int(calc_line_length(topLeft,bottomLeft))
+
+            marker_sides[markerID] = (topLine_length,rightLine_length,bottomLine_length,leftLine_length)
+
+            line_averages = ((topLine_length+bottomLine_length)/2 , (leftLine_length+rightLine_length)/2)
+
+            line_deltas[markerID] = math.sqrt((line_averages[0]-line_averages[1])**2)
+
+            if np.any(line_deltas > max(line_averages)*0.05):
+                g = 0
+                r = 255
+            else:
+                r = 0
+                g = 255
+
+            cv2.line(frame, topLeft, topRight, (0, g, r), 2)
+            cv2.line(frame, topRight, bottomRight, (0, g, r), 2)
+            cv2.line(frame, bottomRight, bottomLeft, (0, g, r), 2)
+            cv2.line(frame, bottomLeft, topLeft, (0, g, r), 2)
 
             # compute and draw the center (x, y)-coordinates of the
             # ArUco marker
@@ -45,7 +78,20 @@ while True:
             cv2.FONT_HERSHEY_SIMPLEX,
             0.5, (0, 255, 0), 2)
 
+            x_label = ids
+            x_space = np.arange(4)
+            # ax.bar(x_space + 0.00 , marker_sides[:,0,0], width = 0.15)
+            # ax.bar(x_space + 0.15 , marker_sides[:,0,1], width = 0.15)
+            # ax.bar(x_space + 0.30 , marker_sides[:,0,2], width = 0.15)
+            # ax.bar(x_space + 0.45 , marker_sides[:,0,3], width = 0.15)
+            plt.bar(x_space + 0.00 , line_deltas[:], width = 0.15, color = (0.5,0.5,0.5,0.5))
+            
+            
+            plt.pause(0.05)
+            
+
     cv2.imshow("Frame",frame)
+        
     key = cv2.waitKey(1) & 0xFF
 
     if key == ord("q"):
